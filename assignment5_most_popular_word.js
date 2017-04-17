9761395281e757ef930488fde43c28647786bbc3
alekmartyanov <alekmartyanov@yandex.ru>
2017-04-18 01:18:18 +0300

    Update assignment5_most_popular_word.js

--- assignment5_most_popular_word.js
+++ assignment5_most_popular_word.js
@@ -1,1 +1,34 @@
+/*
+var wordCounts = { };
+var words = str.split(/\b/);
 
+for(var i = 0; i < words.length; i++)
+    wordCounts["_" + words[i]] = (wordCounts["_" + words[i]] || 0) + 1;
+console.log(wordCounts) 
+*/
+
+function nthMostCommon(string, ammount) {
+    var wordsArray = string.split(/\s/);
+    var wordOccurrences = {}
+    for (var i = 0; i < wordsArray.length; i++) {
+        wordOccurrences['_'+wordsArray[i]] = ( wordOccurrences['_'+wordsArray[i]] || 0 ) + 1;
+    }
+    var result = Object.keys(wordOccurrences).reduce(function(acc, currentKey) {
+        /* you may want to include a binary search here */
+        for (var i = 0; i < ammount; i++) {
+            if (!acc[i]) {
+                acc[i] = { word: currentKey.slice(1, currentKey.length), occurences: wordOccurrences[currentKey] };
+                break;
+            } else if (acc[i].occurences < wordOccurrences[currentKey]) {
+                acc.splice(i, 0, { word: currentKey.slice(1, currentKey.length), occurences: wordOccurrences[currentKey] });
+                if (acc.length > ammount)
+                    acc.pop();
+                break;
+            }
+        }
+        return acc;
+    }, []);
+    return result;
+}
+
+console.log(nthMostCommon(str,1))
