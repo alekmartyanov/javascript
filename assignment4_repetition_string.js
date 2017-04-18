3fe310309398957e8721ed0d74e9316477bc1b92
alekmartyanov <alekmartyanov@yandex.ru>
2017-04-18 23:54:20 +0300

    Update assignment4_repetition_string.js

--- assignment4_repetition_string.js
+++ assignment4_repetition_string.js
@@ -29,3 +29,22 @@ self1="abcabcabc"
 substr1="abc"
 
 console.log(count(self1, substr1));
+
+
+
+function calculate(num) {
+
+    var half = Math.floor(num / 2), // Ensures a whole number <= num.
+        str = '1', // 1 will be a part of every solution.
+        i, j;
+
+    // Determine our increment value for the loop and starting point.
+    num % 2 === 0 ? (i = 2, j = 1) : (i = 3, j = 2);
+
+    for (i; i <= half; i += j) {
+        num % i === 0 ? str += ',' + i : false;
+    }
+
+    str += ',' + num; // Always include the original number.
+    alert(str);
+}
