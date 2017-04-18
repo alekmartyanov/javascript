a0480ed8a9d08716d69b289147f5bfc72879a62b
alekmartyanov <alekmartyanov@yandex.ru>
2017-04-18 10:49:28 +0300

    Update assignment10_Analysis of tweets.js

--- assignment10_Analysis of tweets.js
+++ assignment10_Analysis of tweets.js
@@ -1,5 +1,8 @@
 Most popular tweets 
 https://api.twitter.com/1.1/search/tweets.json?q=topic&result_type=popular
 
+Retweets
+https://api.twitter.com/1.1/statuses/retweets/793802617428160513.json
+
 Places
 http://twitter.com/search?q=place%3A07d9cd6afd884001
