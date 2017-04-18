87137b0addb62e1f764d243bda1273d162067163
alekmartyanov <alekmartyanov@yandex.ru>
2017-04-19 00:53:19 +0300

    Update assignment4_repetition_string.js

--- assignment4_repetition_string.js
+++ assignment4_repetition_string.js
@@ -67,3 +67,17 @@ function AllTheSame(array) {
         return element === first;
     });
 }
+
+function getFactors(integer){
+  var factors = [],
+  quotient = 0;
+
+  for(var i = 1; i <= integer; i++){
+    quotient = integer/i;
+
+    if(quotient === Math.floor(quotient)){
+      factors.push(i); 
+    }
+  }
+  return factors;
+}
