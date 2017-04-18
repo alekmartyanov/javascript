7e1d73894bc7ba2cc32f17acf1e3ca31b1455427
alekmartyanov <alekmartyanov@yandex.ru>
2017-04-18 20:41:28 +0300

    Update assignment4_repetition_string.js

--- assignment4_repetition_string.js
+++ assignment4_repetition_string.js
@@ -1,1 +1,2 @@
 http://stackoverflow.com/questions/6844005/how-can-i-find-the-number-of-overlapping-sequences-in-a-string-with-python
+http://stackoverflow.com/questions/21778799/count-directly-repeated-substring-occurence
