7364e0e203775b19bcb92d056de861f855b42fe3
alekmartyanov <alekmartyanov@yandex.ru>
2017-04-19 11:11:03 +0300

    Update assignment4_repetition_string.js

--- assignment4_repetition_string.js
+++ assignment4_repetition_string.js
@@ -1,131 +1,9 @@
-http://stackoverflow.com/questions/6844005/how-can-i-find-the-number-of-overlapping-sequences-in-a-string-with-python
-http://stackoverflow.com/questions/21778799/count-directly-repeated-substring-occurence
-https://github.com/Na3y/homework_python/blob/master/homework/home4.py
-
-function count(self, substr) {
-  var count = 0
-  var pos = self.indexOf(substr)
-
-  while (pos >= 0) {
-    count += 1
-    pos = self.indexOf(substr, pos + 1)
-  }
-
-  return count
-}
-
-function chunkSubstr(str, size) {
-  var numChunks = Math.ceil(str.length / size),
-      chunks = new Array(numChunks);
-
-  for(var i = 0, o = 0; i < numChunks; ++i, o += size) {
-    chunks[i] = str.substr(o, size);
-  }
-
-  return chunks;
-}
-
-self1="abcabcabc"
-substr1="abc"
-
-console.log(count(self1, substr1));
-
-
-
-function calculate(num) {
-
-    var half = Math.floor(num / 2), // Ensures a whole number <= num.
-        str = '1', // 1 will be a part of every solution.
-        i, j;
-
-    // Determine our increment value for the loop and starting point.
-    num % 2 === 0 ? (i = 2, j = 1) : (i = 3, j = 2);
-
-    for (i; i <= half; i += j) {
-        num % i === 0 ? str += ',' + i : false;
-    }
-
-    str += ',' + num; // Always include the original number.
-    alert(str);
-}
-
-
-Array.prototype.allValuesSame = function() {
-
-    for(var i = 1; i < this.length; i++)
-    {
-        if(this[i] !== this[0])
-            return false;
-    }
-
-    return true;
-}
-
 function AllTheSame(array) {
     var first = array[0];
     return array.every(function(element) {
         return element === first;
     });
 }
-
-function getFactors(integer){
-  var factors = [],
-  quotient = 0;
-
-  for(var i = 1; i <= integer; i++){
-    quotient = integer/i;
-
-    if(quotient === Math.floor(quotient)){
-      factors.push(i); 
-    }
-  }
-  return factors;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function AllTheSame(array) {
-    var first = array[0];
-    return array.every(function(element) {
-        return element === first;
-    });
-}
-
-function chunkSubstr(str, size) {
-    var numChunks = Math.ceil(str.length / size),
-        chunks = new Array(numChunks);
-
-    for(var i = 0, o = 0; i < numChunks; ++i, o += size) {
-        chunks[i] = str.substr(o, size);
-    }
-
-    return chunks;
-}
-
-function count(arr) {
-    len = arr.length
-    for(i=0; i<len; i++){
-        chunkSubstr(self1, arr[i])
-    }
-
-}
-
 function getFactors(integer){
     var factors = [],
         quotient = 0;
@@ -139,34 +17,26 @@ function getFactors(integer){
     }
     return factors;
 }
+function chunkSubstr(str, size) {
+    var numChunks = Math.ceil(str.length / size),
+        chunks = new Array(numChunks);
 
-/*function calculate(str1, num) {
-    var s =str1
-    var half = Math.floor(num / 2), // Ensures a whole number <= num.
-        str = '1', // 1 will be a part of every solution.
-        i, j;
-
-    // Determine our increment value for the loop and starting point.
-    num % 2 === 0 ? (i = 2, j = 1) : (i = 3, j = 2);
-
-    for (i; i <= half; i += j) {
-        num % i === 0 ? str += ',' + i : false;
-        chunkSubstr(s, num)
+    for(var i = 0, o = 0; i < numChunks; ++i, o += size) {
+        chunks[i] = str.substr(o, size);
     }
 
-    str += ',' + num;
+    return chunks;
+}
 
-    // Always include the original number.
-    console.log(str);
-} */
-
-function calculate1(string) {
-    l = string.length
-    getFactors(l)
+function countStringRepetition(str) {
+    v = getFactors(str.length)
+    len = (getFactors(str.length)).length
+    for (i = 0; i < len; i++) {
+        if (AllTheSame(chunkSubstr(str, v[i]))) return ((chunkSubstr(str, v[i])).length)
+        if (AllTheSame(chunkSubstr(str, v[i]))) break
     }
+}
 
-self1="abcabcabc"
+string="abcabcabcabc"
 
-console.log(calculate1(self1))
-
-console.log (AllTheSame(chunkSubstr(self1,3)))
+console.log(countStringRepetition(string))
