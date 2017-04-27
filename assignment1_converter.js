30c247238154f66faa534864b22633e6067b752a
alekmartyanov <alekmartyanov@yandex.ru>
2017-04-27 14:42:49 +0300

    Update assignment1_converter.js

--- assignment1_converter.js
+++ assignment1_converter.js
@@ -2,7 +2,7 @@ function fun1(par){
     try{return (parseInt( par, 2 )) }
     catch (err) {return undefined}
 };
-let abc="abc"
+var abc="abc"
 console.log(fun1(abc));
 
 console.log(fun1('1011010'))
